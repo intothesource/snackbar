2ee2c9ac4fb43b96f5d1d5512f831ef9ada50b79
Ramon Valk <rvalk@intothesource.com>
2019-10-11 13:38:26 +0200

    Added example styling, renamed folder, added snackbar styling

--- example/index.js
+++ example/index.js
@@ -2,7 +2,7 @@ import { Snackbar } from '../snackbar.js';
 
 const frikandelSpeciaal = () => {
     Snackbar('An apple a day and such', 'eat apple', 1000);
-    Snackbar('Bananas something something', 'eat banana', 4000);
+    Snackbar('Bananas something something', 'eat banana', 40000000);
 }
 
 window.frikandelSpeciaal = frikandelSpeciaal;
